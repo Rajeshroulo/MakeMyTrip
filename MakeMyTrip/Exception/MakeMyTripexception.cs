f7ed6073e53659bf7c08724b09a2efc51083f72d
Rajeshroulo <rajraval017@gmail.com>
2020-08-19 18:51:01 +0530

    Added exception class to handle the exceptions in the script

---
+++ MakeMyTrip/Exception/MakeMyTripexception.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MakeMyTrip.exception
+{
+    public class MakeMyTripexception : Exception
+    {
+        public string message;
+
+        public enum ExceptionType
+        {
+            ELEMENT_NOT_FOUND,TIMED_OUT
+        }
+        public  ExceptionType type;
+
+        public MakeMyTripexception(ExceptionType type,string message):base(message)
+        {
+            this.type = type;
+            this.message = message;
+        }
+
+    }
+}
